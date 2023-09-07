be851be5774699979ded4a524393af40d04eb751
guajardoml <141788302+guajardoml@users.noreply.github.com>
2023-09-06 21:12:42 -0600

    Semana_5_Laboratorio_2002423_Mia_Guajardo

---
+++ Semana_5_Laboratorio_2002423_Mia_Guajardo/L1MLGJ2002423/L1MLGJ2002423/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hola Mundo");
+        //Console.WriteLine("Soy Mia");
+
+        //Console.Write("Hola Mundo ");
+        //Console.Write("Soy Mia");
+
+        ///*comentario de programa*/
+        //Console.ReadKey();
+        //Console.ReadLine();
+
+        Console.WriteLine("Ingrese su nombre: ");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("soy " + Nombre);
+
+        /*comentario de programa*/
+
+        Console.Write("Hola Mundo ");
+        Console.Write("soy " + Nombre);
+        Console.ReadKey();
+
+    }
+
+}
---
+++ Semana_5_Laboratorio_2002423_Mia_Guajardo/T1_MLGJ_2002423/T1_MLGJ_2002423/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi segundo programa ");
+
+        string sNombre;
+        sbyte sEdad;
+        string sCarrera;
+        string SCarne;
+
+        Console.WriteLine("Ingrese su nombre: ");
+        sNombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad: ");
+        sEdad = sbyte.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese se carrera: ");
+        sCarrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carne de la universidad: ");
+        SCarne = Console.ReadLine();
+
+        Console.Clear();
+
+        Console.WriteLine("Nombre: " + sNombre);
+        Console.WriteLine("Edad: " + sEdad);
+        Console.WriteLine("Carrera: " + sCarrera);
+        Console.WriteLine("Carne: " + SCarne);
+
+        Console.ReadKey();
+        Console.Clear();
+
+        Console.WriteLine("Soy " + sNombre + ", tengo " + sEdad + " años y estudio la carrera de " + sCarrera + ", mi numero de carne es: " + SCarne);
+
+        Console.ReadKey();
+
+
+
+    }
+}
