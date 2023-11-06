3da85f72698788a58265f240454b882ec17cd2a9
guajardoml <141788302+guajardoml@users.noreply.github.com>
2023-11-06 09:12:52 -0600

    Add files via upload

---
+++ SEMANA_14_LABORATORIO_2002423_MIAGUAJARDO/L14_MLGJ_2002423/L14_MLGJ_2002423/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int opcionmen = 0;
+        
+        do
+        {
+
+
+            Console.WriteLine("Por favor Ingrese 7 numeros enteros");
+            int[] numeros = new int[7];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Numero: ");
+                numeros[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine("El numero mayor ingresado es: " + numeros.Max());
+            Console.WriteLine("El numero menos ingresado es: " + numeros.Min());
+            int positivos = 0;
+            int negativos = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > 0)
+                {
+                    positivos++;
+                }
+                else
+                {
+                    negativos++;
+                }
+            }
+
+            Console.WriteLine("Usted ingreso {0} numeros positivos", positivos);
+            Console.WriteLine("Usted ingreso {0} numeros negativos", negativos);
+
+            Console.WriteLine("Desea ingresar otra secuencia de numeros?");
+            Console.WriteLine("Si lo desea, presiones 0, si no lo desea, presione 1.");
+            if (opcionmen == 1)
+            {
+                Console.WriteLine("Usted ingreso los siguiente numeros");
+                for (int i = 0; i < numeros.Length; i++) {
+                    Console.WriteLine(numeros[i]);
+                }
+            }
+        } while (opcionmen == 0);
+
+        
+
+    }
+}
